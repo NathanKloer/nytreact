87e17a37309307fd96e1e37e8a293be565ed005d
Nathan <nathan.kloer@gmail.com>
2019-02-14 15:27:14 -0500

    Updates

---
+++ precache-manifest.b8f3bb9d0b317b2a258721242bb50cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nytreact.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4020a14ab16c98611234",
+    "url": "/nytreact.git/static/js/runtime~main.4020a14a.js"
+  },
+  {
+    "revision": "d1dacd9aa99b7ab13c33",
+    "url": "/nytreact.git/static/js/main.d1dacd9a.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/nytreact.git/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "d1dacd9aa99b7ab13c33",
+    "url": "/nytreact.git/static/css/main.56e66f83.chunk.css"
+  },
+  {
+    "revision": "dcd369e5fc35ed9ddb51c927624154e6",
+    "url": "/nytreact.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nytreact.git/precache-manifest.b8f3bb9d0b317b2a258721242bb50cf8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nytreact.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
